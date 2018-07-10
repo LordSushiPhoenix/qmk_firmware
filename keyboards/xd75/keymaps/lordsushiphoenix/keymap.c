ede6ebb93dd5b6575ab00efa93fe1858b92af565
ERIK-DESKTOP\Erik <erik.christensen108@gmail.com>
2018-07-09 23:12:55 -0400

    commented planned changes

--- keyboards/xd75/keymaps/lordsushiphoenix/keymap.c
+++ keyboards/xd75/keymaps/lordsushiphoenix/keymap.c
@@ -35,17 +35,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
- [_QW] = { /* QWERTY */
-  { KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_MINS, KC_BSLS, KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_HOME  },
+ [_QW] = { /* QWERTY, */
+  { KC_GESC/*this key breaks task manager shortcut*/,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_MINS, KC_BSLS, KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_HOME  },
   { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_P7,   KC_P8,   KC_P9,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_END   },
   { MO(_FN), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_P4,   KC_P5,   KC_P6,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOTE },
   { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_P1,   KC_P2,   KC_P3,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP, KC_SLASH },
   { KC_LCTL, KC_LGUI, KC_LALT, MO(_FN), KC_DEL,  KC_SPC,  KC_NLCK, KC_P0,   KC_PDOT, KC_BSPC, KC_ENT,  MO(_FN), KC_LEFT, KC_DOWN, KC_RIGHT  },
  },
 
-/* FUNCTION
+/* FUNCTION   need to fix more forgotten keys, also shift f row over to accomodate brackets
  * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | GRV    | F1     | F2     | F3     | F4     | F5     | F6     | F7     | F8     | F9     | F10    | F11    | F12    |        | PGUP    |
+ * | GRV    | F1     | F2     | F3     | F4     | F5     | F6     | F7     | F8     | F9     | F10    | F11    | F12    |        | PGUP   |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * | CAPSLK | CALC   | MYCOMP | MAIL   | RGB HD | RGB HI | P7     | P8     | P9     | -      |        |        | [      | ]      | PGDN   |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
@@ -64,6 +64,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_TRNS, KC_MUTE, KC_VOLU, KC_APP,  RGB_VAD, RGB_VAI, KC_TRNS, KC_TRNS, KC_TRNS,   KC_PENT, _______, _______, KC_MUTE, KC_VOLU, KC_MPLY  },
   { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_RMOD,RGB_MOD, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT  },
  }
+
+ //add quick navigation and meme layers, will need macros
 };
 
 const uint16_t PROGMEM fn_actions[] = {
