65d8ec4be13037ea97b49d91677b52f7b2c46bfc
ERIK-DESKTOP\Erik <19640845+LordSushiPhoenix@users.noreply.github.com>
2018-07-10 23:02:11 -0400

    added capslock to the momentary layer toggle, need to move numlock since i keep hitting it instead of space. 2u keys would be nice

--- keyboards/xd75/keymaps/lordsushiphoenix/keymap.c
+++ keyboards/xd75/keymaps/lordsushiphoenix/keymap.c
@@ -36,11 +36,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QW] = { /* QWERTY, */
-  { KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_BSLS, KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_HOME  },
-  { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_P7,   KC_P8,   KC_P9,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_END   },
-  { MO(_FN), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_P4,   KC_P5,   KC_P6,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOTE },
-  { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_P1,   KC_P2,   KC_P3,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLASH },
-  { KC_LCTL, KC_LGUI, KC_LALT, KC_DEL,  KC_SPC,  KC_SPC,  KC_NLCK, KC_P0,   KC_PDOT, KC_BSPC, KC_ENT,  MO(_FN), KC_LEFT, KC_DOWN, KC_RIGHT },
+  { KC_GESC,          KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_BSLS, KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_HOME  },
+  { KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_P7,   KC_P8,   KC_P9,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_END   },
+  { LT(_FN, KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_P4,   KC_P5,   KC_P6,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOTE },
+  { KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_P1,   KC_P2,   KC_P3,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLASH },
+  { KC_LCTL,          KC_LGUI, KC_LALT, KC_DEL,  KC_SPC,  KC_SPC,  KC_NLCK, KC_P0,   KC_PDOT, KC_BSPC, KC_ENT,  MO(_FN), KC_LEFT, KC_DOWN, KC_RIGHT },
  },
 
 /* FUNCTION
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_CAPS, KC_ESC,  KC_UP,   KC_TRNS,  RGB_HUD, RGB_HUI, KC_TRNS, KC_TRNS, KC_TRNS, KC_PMNS, KC_PSLS, _______, KC_PSCR, KC_SLCK, KC_PGDN },
   { KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT, RGB_SAD, RGB_SAI, KC_TRNS, KC_TRNS, KC_TRNS, KC_PPLS, KC_PAST, RESET,   _______, _______, _______ },
   { KC_TRNS, KC_MUTE, KC_VOLU, KC_APP,   RGB_VAD, RGB_VAI, KC_TRNS, KC_TRNS, KC_TRNS, KC_PENT, _______, _______, KC_MUTE, KC_VOLU, KC_MPLY },
-  { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  RGB_RMOD,RGB_MOD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT },
+  { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  RGB_RMOD,RGB_MOD, KC_NLCK, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT },
  }
 
  //add quick navigation and meme layers, will need macros
