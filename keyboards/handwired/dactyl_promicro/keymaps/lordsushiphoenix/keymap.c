fc5d89509906a555ed2668724cd2f48322b2594d
lordsushiphoenix <19640845+LordSushiPhoenix@users.noreply.github.com>
2019-02-27 08:45:52 -0500

    added a del key

--- keyboards/handwired/dactyl_promicro/keymaps/lordsushiphoenix/keymap.c
+++ keyboards/handwired/dactyl_promicro/keymaps/lordsushiphoenix/keymap.c
@@ -14,12 +14,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
   [_QW] = LAYOUT_6x6(
-     KC_GESC,           KC_1  ,  KC_2  ,  KC_3  ,  KC_4  , KC_5  ,      KC_6  ,  KC_7   , KC_8   ,       KC_9   ,       KC_0  ,  KC_HOME,
+     KC_GESC,          KC_1  ,  KC_2  ,  KC_3  ,  KC_4  , KC_5  ,      KC_6  ,  KC_7   , KC_8   ,       KC_9   ,       KC_0  ,  KC_HOME,
      KC_TAB,           KC_Q  ,  KC_W  ,  KC_E  ,  KC_R  , KC_T  ,      KC_Y  ,  KC_U   , KC_I   ,       KC_O   ,       KC_P  ,  KC_END,
      LT(_FN, KC_CAPS), KC_A  ,  KC_S  ,  KC_D  ,  KC_F  , KC_G  ,      KC_H  ,  KC_J   , LT(_NV, KC_K), LT(_SM, KC_L), KC_SCLN, KC_QUOT,
      KC_LSFT,          KC_Z  ,  KC_X  ,  KC_C  ,  KC_V  , KC_B  ,      KC_N  ,  KC_M   , KC_COMM,       KC_DOT ,       KC_SLSH, KC_BSLASH,
-     KC_LCTL,          KC_LGUI, KC_LALT , KC_DOWN, KC_NO,                        KC_LEFT, KC_DOWN,       KC_UP,         KC_RIGHT,MO(_FN),
-     KC_LGUI,          KC_ESC,  KC_RALT, KC_RGUI, KC_TAB ,KC_SPC,      KC_LCTL, KC_RCTL, KC_BSPC,       KC_ENT,        KC_LSFT, KC_RSFT
+     KC_LCTL,          KC_LGUI, KC_LALT, KC_DOWN, KC_DEL,                       KC_LEFT, KC_DOWN,       KC_UP,         KC_RIGHT,MO(_FN),
+     KC_LGUI,          KC_ESC,  KC_RALT, KC_LGUI, KC_TAB ,KC_SPC,      KC_LCTL, KC_RCTL, KC_BSPC,       KC_ENT,        KC_LSFT, KC_RSFT
                                                              
   ),
 
